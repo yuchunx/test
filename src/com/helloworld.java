04a5b10b0152ac085bac30e33613541232b2af57
shelfer <xyc18381406463@163.com>
2020-07-06 02:14:41 +0800

    第三次了

--- src/com/helloworld.java
+++ src/com/helloworld.java
@@ -4,5 +4,6 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("这是我第一个idealU程序");
         System.out.println("修改一次");
+        System.out.println("第三次");
     }
 }
